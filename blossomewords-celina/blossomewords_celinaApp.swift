9af8404b1a2078002b58948bdfade9b4d2aa8f94
Celina Tong <celinanatong@gmail.com>
2023-08-02 11:53:34 -0700

    Initial Commit

---
+++ blossomewords-celina/blossomewords_celinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  blossomewords_celinaApp.swift
+//  blossomewords-celina
+//
+//  Created by Celina Tong on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct blossomewords_celinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
