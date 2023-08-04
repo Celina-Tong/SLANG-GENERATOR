9e7d2abbaf6b56baec44dd9feb319e98d3c5227e
Celina Tong <celinanatong@gmail.com>
2023-08-03 22:43:49 -0700

    random image generator

--- blossomewords-celina/ContentView.swift
+++ blossomewords-celina/ContentView.swift
@@ -5,19 +5,55 @@
 //  Created by Celina Tong on 8/2/23.
 //
 
+//Slang genrator
+
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+ @State var showDetails = false
+    var slangs = ["slangone","slangtwo","slangthree","slangfour","slangfive"]
+@State private var randomIndex = 0
+    var imageName = ""
+    
     var body: some View {
+        
+        ZStack{
+            
+            (Color("YELLOW"))
+                .ignoresSafeArea()
+                .overlay(
+                    
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+           
+            Text("Slang/Common Phrases")
+                .font(.title)
+                .multilineTextAlignment(.center)
+            
+            if showDetails {
+                Image(slangs[randomIndex])
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+            
+            Group{
+                Button("Click here for slang word of the day", action: {
+                    self.showDetails.toggle()
+                    if self.showDetails == true {
+                        self.randomIndex = Int.random(in:0..<slangs.count)
+                    }
+                })
+                
+            }
+             }
+        .padding(.top)
+        )
         }
-        .padding()
+        }
     }
-}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
